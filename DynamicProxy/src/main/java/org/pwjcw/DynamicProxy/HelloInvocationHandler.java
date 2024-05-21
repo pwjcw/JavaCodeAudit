20736f8380781cf8e06b539ff18ea9c565bb9e6d
pwjwc <you@example.com>
2024-05-21 23:19:40 +0800

    update

--- DynamicProxy/src/main/java/org/pwjcw/DynamicProxy/HelloInvocationHandler.java
+++ DynamicProxy/src/main/java/org/pwjcw/DynamicProxy/HelloInvocationHandler.java
@@ -10,7 +10,9 @@ public class HelloInvocationHandler implements InvocationHandler {
     }
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("执行前");
         Object invoke = method.invoke(target, args);
+        System.out.println("执行后");
         return invoke;
     }
 }
--- DynamicProxy/src/main/java/org/pwjcw/DynamicProxy/Test.java
+++ DynamicProxy/src/main/java/org/pwjcw/DynamicProxy/Test.java
@@ -1,19 +1,23 @@
 package org.pwjcw.DynamicProxy;
 
 import org.pwjcw.Hello;
+import sun.applet.Main;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class Test {
-    Hello hello=new Hello() {
-        @Override
-        public void Say(String name) {
-            System.out.println("Say Hello");
-        }
-    };
-    HelloInvocationHandler helloInvocationHandler =new HelloInvocationHandler(hello);
-    //创建代理对象
-    Proxy.newProxyInstance();
+    public static void main(String[] args) {
+        Hello hello=new Hello() {
+            @Override
+            public void Say(String name) {
+                System.out.println("Say Hello");
+            }
+        };
+        HelloInvocationHandler helloInvocationHandler =new HelloInvocationHandler(hello);
+        //创建代理对象
+        Hello HelloProxy = (Hello) Proxy.newProxyInstance(Test.class.getClassLoader(), new Class[]{Hello.class}, helloInvocationHandler);
+        HelloProxy.Say("whoami");
+    }
 }
